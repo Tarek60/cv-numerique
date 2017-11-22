7dcbd541d3010b8766289cc15a71c9c6e0fb2496
Tarek <tarek.elouatar@gmail.com>
2017-11-22 13:43:49 +0100

    cv numerique

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+$('.croix1').click(function(){
+  $('.block1').hide();
+});
+$('.croix2').click(function(){
+  $('.block2').hide();
+});
+$('.croix3').click(function(){
+  $('.block3').hide();
+});
+$('.croix4').click(function(){
+  $('.block4').hide();
+});
+$('.croix5').click(function(){
+  $('.block5').hide();
+});
+$('.agrandir1').click(function(){
+  $('.block1').css('height', '+=100px');
+});
